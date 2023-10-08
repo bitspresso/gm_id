e0d2b067080e0d4c23c368fa776f6575cc64f5e4
Enrico <73340659+Enrypase@users.noreply.github.com>
2023-10-08 09:45:33 +0200

    fix: minor UI fixes

--- website/src/components/common/user.jsx
+++ website/src/components/common/user.jsx
@@ -1,12 +1,28 @@
 import Image from "next/image";
+import { useState } from "react";
 
+function Skill({ name, xp }) {
+  return (
+    <div className="">
+      <p>{name}:</p>
+      <div className="relative w-full h-8 rounded-full border-2 border-solid border-black overflow-hidden">
+        <div
+          className={`absolute left-0 bg-black h-full`}
+          style={{
+            width: (xp / 1000) * 100,
+          }}
+        />
+      </div>
+    </div>
+  );
+}
 export default function User({ data }) {
   const [open, setOpen] = useState(false);
   return (
     <>
       {open && (
-        <div className="absolute z-10 flex w-[350px] h-[550px] justify-center items-center top-0 left-0">
-          <div className="relative w-[90%] h-[90%] bg-white rounded-xl p-4">
+        <div className="absolute z-10 flex w-full h-fulll justify-center items-center top-[50%] -translate-y-[50%] left-0 text-black">
+          <div className="relative w-[90%] h-[90%] bg-white rounded-xl p-4 pb-12">
             <button
               className="absolute right-4 top-4"
               onClick={() => setOpen(false)}
@@ -15,7 +31,7 @@ export default function User({ data }) {
             </button>
             <div className="flex flex-col items-center w-full h-full">
               <h2>{data.address}</h2>
-              <Image
+              <img
                 className="h-32 w-32 rounded-full"
                 src={data.pfp}
                 alt={data.address}
@@ -26,7 +42,7 @@ export default function User({ data }) {
                     key={`trait_holder_${i}`}
                     className="relative group w-16 h-16"
                   >
-                    <Image className="w-full h-full" src={el} alt="Trait" />
+                    <img className="w-full h-full" src={el} alt="Trait" />
                     <div className="absolute bottom-0 left-0 w-full h-full bg-white group-hover:visible invisible">
                       <p className="">Name</p>
                     </div>
@@ -34,10 +50,9 @@ export default function User({ data }) {
                 ))}
               </div>
               <div className="flex flex-col w-full">
-                {data.skills.map(
-                  (el, i) => ""
-                  //<Skill key={`skill_${i}`} name={el.name} xp={el.xp} />
-                )}
+                {data.skills.map((el, i) => (
+                  <Skill key={`skill_${i}`} name={el.name} xp={el.xp} />
+                ))}
               </div>
               <div className="absolute bottom-0 left-0 flex gap-2">
                 {data.socials.map((el, i) => (
@@ -47,7 +62,8 @@ export default function User({ data }) {
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <Image
+                    <img
+                      width={"60px"}
                       className="w-10 h-10"
                       src={
                         "https://imgs.search.brave.com/1fQWh1HTRWcKfQHLhg0KeQEAMeaeGDhSJbBrEYsen3g/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9naXRo/dWIuZ2l0aHViYXNz/ZXRzLmNvbS9pbWFn/ZXMvbW9kdWxlcy9s/b2dvc19wYWdlL0dp/dEh1Yi1NYXJrLnBu/Zw"
@@ -63,12 +79,7 @@ export default function User({ data }) {
       )}
       <button onClick={() => setOpen(true)} href="/">
         <div className="relative w-16 h-16 rounded-full overflow-hidden">
-          <Image className="" src={data.pfp} alt={data.address} />
-          <div className="pl-4 pr-4 flex gap-1 absolute bottom-0 left-0 bg-transparent h-5 w-full">
-            {data.traits.map((el, i) => (
-              <Image className="w-4 h-4" key={`trait_${i}`} src={el} alt="T" />
-            ))}
-          </div>
+          <img className="" src={data.pfp} alt={data.address} />
         </div>
       </button>
     </>
